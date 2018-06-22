10313c312f1600096564f776f84b6c572c78239d
JeonSeongBae <tjdqo6042@naver.com>
2018-06-22 19:26:17 +0900

    아두이노를 위한 클래스 생성

---
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/Arduino.java
@@ -0,0 +1,27 @@
+package kaiser.airqualityapplication;
+
+/**
+ * Created by tjdqo_000 on 2018-06-22.
+ */
+
+public class Arduino {
+    //SD2Card card;
+
+    public void setup(){
+
+    }
+    public void loop(){
+
+    }
+    public byte readSensor(){
+        return 0;
+    }
+    public void saveData(){
+    }
+    public boolean connectBLE(){
+        return true;
+    }
+    public void sendData(){
+
+    }
+}
