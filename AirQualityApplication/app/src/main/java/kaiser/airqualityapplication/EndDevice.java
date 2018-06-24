e4cce7df44556e3b53c20b718be166e74c6f196a
JeonSeongBae <tjdqo6042@naver.com>
2018-06-24 21:58:24 +0900

    EndDevice 생성자 및 접근자 생성

--- AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/EndDevice.java
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/EndDevice.java
@@ -1,16 +1,57 @@
 package kaiser.airqualityapplication;
 
+import java.sql.Time;
+
 /**
- * Created by tjdqo_000 on 2018-06-21.
+ * Created by tjdqo_000 on 2018-06-02.
  */
 
-class EndDevice {
+public class EndDevice {
+    private String ID;
+    private double[] density;
     private String color;
+    private double latitude;
+    private double hardness;
+    Time time;
 
-    public EndDevice(String id, int i, double v, double v1) {
+    public EndDevice(String ID, int density, double latitude, double hardness){
+        this.ID = ID;
+        this.density = new double[10];
+        this.color = "";
+        this.latitude = latitude;
+        this.hardness = hardness;
+        this.time = new Time(1);
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setHardness(double hardness) {
+        this.hardness = hardness;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getHardness() {
+        return hardness;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
     }
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getColor() {
+        return color;
     }
 }
