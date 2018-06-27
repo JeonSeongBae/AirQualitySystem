ad1765684536e08a69d270e8ad24263dff417f9c
JeonSeongBae <tjdqo6042@naver.com>
2018-06-27 17:51:12 +0900

    EndDevice Class를 Mapping하여 FireBase에 저장

--- AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/EndDevice.java
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/EndDevice.java
@@ -1,5 +1,10 @@
 package kaiser.airqualityapplication;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by tjdqo_000 on 2018-06-21.
  */
@@ -7,10 +12,71 @@ package kaiser.airqualityapplication;
 class EndDevice {
     private String color;
 
-    public EndDevice(String id, int i, double v, double v1) {
+    private String ID;
+    private double density;
+    private double latitude;
+    private double longitude;
+
+    public EndDevice(){
+        // Defalut constructor required for calls to DataSnapshot.getValue(EndDevice.class)
+    }
+
+    public EndDevice(String color, String ID, double density, double latitude, double longitude) {
+        this.color = color;
+        this.ID = ID;
+        this.density = density;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("ID", ID);
+        result.put("density", density);
+        result.put("latitude", latitude);
+        result.put("longitude", longitude);
+
+        return result;
     }
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public void setDensity(double density) {
+        this.density = density;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public double getDensity() {
+        return density;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
     }
 }
--- AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/MainActivity.java
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/MainActivity.java
@@ -2,16 +2,59 @@ package kaiser.airqualityapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<EndDevice> list_EndDevice;
     private BLE ble;
+    private TextView textViewID;
+    private EditText editTextID;
+    private TextView textViewDensity;
+    private EditText editTextDensity;
+    private TextView textViewLatitude;
+    private EditText editTextLatitude;
+    private TextView textViewLongitude;
+    private EditText editTextLongitude;
+    private Button buttonSaveEndDevice;
+    private FirebaseDatabase firebaseDatabase;
+    private String key;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TextView와 EditText 생성
+        textViewID = findViewById(R.id.textViewID);
+        editTextID = findViewById(R.id.editTextID);
+        textViewDensity = findViewById(R.id.textViewDensity);
+        editTextDensity = findViewById(R.id.editTextDensity);
+        textViewLatitude = findViewById(R.id.textViewLatitude);
+        editTextLatitude = findViewById(R.id.editTextLatitude);
+        textViewLongitude = findViewById(R.id.textViewLongitude);
+        editTextLongitude = findViewById(R.id.editTextLongitude);
+        buttonSaveEndDevice = findViewById(R.id.buttonSaveEndDevice);
+
+        // 데이터베이스 Instance 생성
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // Button 클릭 기능 생성
+        buttonSaveEndDevice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeNewEndDevice(editTextID.getText().toString(), editTextDensity.getText().toString(), editTextLatitude.getText().toString(), editTextLongitude.getText().toString());
+            }
+        });
+
         list_EndDevice = new ArrayList<>();
         initNode();
         ble = new BLE();
@@ -23,6 +66,21 @@ public class MainActivity extends AppCompatActivity {
 
         writeData(receiveData());
     }
+
+    private void writeNewEndDevice(String ID, String density, String latitude, String longitude) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+
+        EndDevice endDevice = new EndDevice(null,ID, Double.valueOf(density).doubleValue(), Double.valueOf(latitude).doubleValue(), Double.valueOf(longitude).doubleValue());
+        Map<String, Object> postValues = endDevice.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/Node/" + "공대 5호관", postValues);
+        // childUpdates.put("/user-posts/" + key, postValues);
+
+        firebaseDatabase.getReference().updateChildren(childUpdates);
+    }
+
     // 앱 투 DB
     private void writeData(double data) {
         // data를 블루투스 연결된 ID로 update
@@ -37,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private EndDevice readData(String ID){
-        EndDevice endDevice = new EndDevice("ID", 50, 0.0, 0.0);
+        EndDevice endDevice = new EndDevice(null,"ID", 50, 0.0, 0.0);
         return null;
     }
 
@@ -64,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean initNode() {
         // 데이터 베이스에서 노드 정보 불러옴
         // 정보를 기반으로 class 생성
-        EndDevice endDevice = new EndDevice("ID", 50,0.0,0.0);
+        EndDevice endDevice = new EndDevice(null,"ID", 50,0.0,0.0);
         decideColor(endDevice);
         mapPIN(endDevice);
         list_EndDevice.add(endDevice);
