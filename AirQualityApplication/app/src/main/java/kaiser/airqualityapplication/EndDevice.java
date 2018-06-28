a761dbf1918b9ec55a96409ef3bd7827743c7708
JeonSeongBae <tjdqo6042@naver.com>
2018-06-28 14:35:58 +0900

    버튼을 이용한 사진추가 및 데이터베이스 등록

--- AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/EndDevice.java
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/EndDevice.java
@@ -1,44 +1,17 @@
 package kaiser.airqualityapplication;
 
-<<<<<<< HEAD
 import com.google.firebase.database.Exclude;
 
 import java.util.HashMap;
 import java.util.Map;
-=======
-import java.sql.Time;
->>>>>>> 45cc0a9a03cf49f66092d1daf0c57d0e437b34b3
 
 /**
  * Created by tjdqo_000 on 2018-06-02.
  */
 
 public class EndDevice {
-    private String ID;
-    private double[] density;
+
     private String color;
-    private double latitude;
-    private double hardness;
-    Time time;
-
-    public EndDevice(String ID, int density, double latitude, double hardness){
-        this.ID = ID;
-        this.density = new double[10];
-        this.color = "";
-        this.latitude = latitude;
-        this.hardness = hardness;
-        this.time = new Time(1);
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public void setHardness(double hardness) {
-        this.hardness = hardness;
-    }
-
-<<<<<<< HEAD
     private String ID;
     private double density;
     private double latitude;
@@ -65,25 +38,12 @@ public class EndDevice {
         result.put("longitude", longitude);
 
         return result;
-=======
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getHardness() {
-        return hardness;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
->>>>>>> 45cc0a9a03cf49f66092d1daf0c57d0e437b34b3
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
-<<<<<<< HEAD
     public void setID(String ID) {
         this.ID = ID;
     }
@@ -104,13 +64,10 @@ public class EndDevice {
         return color;
     }
 
-=======
->>>>>>> 45cc0a9a03cf49f66092d1daf0c57d0e437b34b3
     public String getID() {
         return ID;
     }
 
-<<<<<<< HEAD
     public double getDensity() {
         return density;
     }
@@ -121,9 +78,5 @@ public class EndDevice {
 
     public double getLongitude() {
         return longitude;
-=======
-    public String getColor() {
-        return color;
->>>>>>> 45cc0a9a03cf49f66092d1daf0c57d0e437b34b3
     }
 }
--- AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/MainActivity.java
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/MainActivity.java
@@ -1,13 +1,35 @@
 package kaiser.airqualityapplication;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +46,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextLongitude;
     private Button buttonSaveEndDevice;
     private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference firebaseDatabaseRef;
     private String key;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference firebaseStorageRef;
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private ImageView imageViewUpload;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +70,43 @@ public class MainActivity extends AppCompatActivity {
         textViewLongitude = findViewById(R.id.textViewLongitude);
         editTextLongitude = findViewById(R.id.editTextLongitude);
         buttonSaveEndDevice = findViewById(R.id.buttonSaveEndDevice);
+        buttonChoose = findViewById(R.id.buttonChoose);
+        buttonUpload = findViewById(R.id.buttonUpload);
+        imageViewUpload = findViewById(R.id.imageViewUpload);
 
         // 데이터베이스 Instance 생성
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabaseRef = firebaseDatabase.getReference();
+
+        // 스토리지 Instance 생성
+        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseStorageRef = firebaseStorage.getReference();
 
         // Button 클릭 기능 생성
         buttonSaveEndDevice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 writeNewEndDevice(editTextID.getText().toString(), editTextDensity.getText().toString(), editTextLatitude.getText().toString(), editTextLongitude.getText().toString());
+            }
+        });
+        
+        // Choose Button 클릭 기능 생성
+        buttonChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Select Image
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
+            }
+        });
+        
+        // Upload Button 클릭 기능 생성
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile();
             }
         });
 
@@ -65,6 +122,73 @@ public class MainActivity extends AppCompatActivity {
         writeData(receiveData());
     }
 
+    //결과 처리
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //request코드가 0이고 OK를 선택했고 data에 뭔가가 들어 있다면
+        if(requestCode == 0 && resultCode == RESULT_OK){
+            filePath = data.getData();
+            Log.d("MainActivity", "uri:" + String.valueOf(filePath));
+            try {
+                //Uri 파일을 Bitmap으로 만들어서 ImageView에 집어 넣는다.
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageViewUpload.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //upload the file
+    private void uploadFile() {
+        //업로드할 파일이 있으면 수행
+        if (filePath != null) {
+            //업로드 진행 Dialog 보이기
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("업로드중...");
+            progressDialog.show();
+
+            //Unique한 파일명을 만들자.
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMHH_mmss");
+            Date now = new Date();
+            String filename = formatter.format(now) + ".jpg";
+            //storage 주소와 폴더 파일명을 지정해 준다.
+            StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://lg01-ba3b9.appspot.com").child(filename);
+
+            //올라가거라...
+            storageRef.putFile(filePath)
+                    //성공시
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss(); //업로드 진행 Dialog 상자 닫기
+                            Toast.makeText(getApplicationContext(), "업로드 완료!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //실패시
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    //진행중
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests") //이걸 넣어 줘야 아랫줄에 에러가 사라진다. 넌 누구냐?
+                                    double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                            //dialog에 진행률을 퍼센트로 출력해 준다
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void writeNewEndDevice(String ID, String density, String latitude, String longitude) {
         // Create new post at /user-posts/$userid/$postid and at
         // /posts/$postid simultaneously
@@ -76,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
         childUpdates.put("/Node/" + "공대 5호관", postValues);
         // childUpdates.put("/user-posts/" + key, postValues);
 
-        firebaseDatabase.getReference().updateChildren(childUpdates);
+        firebaseDatabaseRef.updateChildren(childUpdates);
     }
 
     // 앱 투 DB
