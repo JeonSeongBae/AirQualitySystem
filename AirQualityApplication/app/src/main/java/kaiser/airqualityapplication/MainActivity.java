e123e95ee7f947a9fec387d24d810b2972ac8f9b
JeonSeongBae <tjdqo6042@naver.com>
2018-07-14 17:53:22 +0900

    ble activity

--- AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/MainActivity.java
+++ AirQualityApplication/app/src/main/java/kaiser/airqualityapplication/MainActivity.java
@@ -1,24 +1,16 @@
 package kaiser.airqualityapplication;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -26,21 +18,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -108,6 +90,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 printNode();
+                Intent intent = new Intent(MainActivity.this, BLEActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -115,10 +99,10 @@ public class MainActivity extends AppCompatActivity {
 
         if(initNode()){
             // 초기화 성공
-            Toast.makeText(this,"Success initNode",Toast.LENGTH_LONG);
+            Toast.makeText(this,"Success initNode",Toast.LENGTH_LONG).show();
         }else{
             // 초기화 실패
-            Toast.makeText(this,"Failure initNode",Toast.LENGTH_LONG);
+            Toast.makeText(this,"Failure initNode",Toast.LENGTH_LONG).show();
         }
         updateNode();
     }
@@ -147,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
+                Log.i("TAG", "msg");
             }
 
             @Override
@@ -185,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean initNode() {
         // 데이터 베이스에서 노드 정보 불러옴
         // 정보를 기반으로 class 생성
-        firebaseDatabaseRef.child("/registedNode/").child("공대5호관").setValue(new EndDevice("red", "공대5호관", 41.0, 36.366002, 127.345320));
+        firebaseDatabaseRef.child("/registedNode/").child("00").setValue(new EndDevice("red", "공대5호관", 41.0, 36.366002, 127.345320));
         firebaseDatabaseRef.child("/registedNode/").child("충대정문").setValue(new EndDevice("blue", "충대정문", 12.0, 36.366125, 127.343891));
         firebaseDatabaseRef.child("/registedNode/").child("공대1호관").setValue(new EndDevice("yellow", "공대1호관", 45.0, 36.367810, 127.341391));
         firebaseDatabaseRef.child("/registedNode/").child("교양관").setValue(new EndDevice("red", "교양관", 25.0, 36.368447, 127.345729));
